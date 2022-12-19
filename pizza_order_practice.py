e3f2e2e3399d2cfef8d0368f0f863bb94333d244
sudharaD <sudharad7@gmail.com>
2022-12-19 23:17:39 +0530

    python pizza deliveries

---
+++ pizza_order_practice.py
@@ -0,0 +1,29 @@
+print("Welcome to python pizza deliveries!")
+while True:
+    try:
+        size = input("What size pizza do ypu want? S, M, or L ")
+        add_pepperoni = input("Do you want pepperoni? Y or N ")
+        extra_cheese = input("Do you want extra cheese? Y or N ")
+
+        bill = 0
+
+        if size == "S":
+            bill = 15
+            if add_pepperoni == "Y":
+                bill += 2
+        else:
+            if size == "M":
+                bill = 20
+            else:
+                bill = 25
+            if add_pepperoni == "Y":
+                bill += 3
+
+        if extra_cheese == "Y":
+            bill += 1
+
+        print(f"Your final bill is: {bill}$")
+
+    except:
+        print("Something Wrong!")
+        
