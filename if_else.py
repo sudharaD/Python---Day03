6250676723d59002e49d3c6306b9b022c816c53f
sudharaD <sudharad7@gmail.com>
2022-12-19 21:45:48 +0530

    if & else nested if

--- if_else.py
+++ if_else.py
@@ -2,7 +2,19 @@ print("Welcome to the rollercoaster!")
 
 height = int(input("What is your height in cm? "))
 
-if height > 120:
+if height >= 120:
     print("Can ride the rollercoaster")
+
+    age = int(input("How old are you? "))
+
+    if age >= 18:
+        print("12$ Ticket")
+    else:
+        print("7$ Ticket")
+
 else:
     print("Can't ride")
+
+
+    # == -> equal to
+    # != -> not equal to
